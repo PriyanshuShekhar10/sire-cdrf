c3c1c59feaa6e751c72177c98421403cb769c0db
code-death <poorvanshkavta938@gmail.com>
2024-03-31 12:53:46 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path={'/cdrf'} element={<ProfessorTable />} />
+            <Route path={'/'} element={<ProfessorTable />} />
         </Routes>
     </BrowserRouter>
   );
