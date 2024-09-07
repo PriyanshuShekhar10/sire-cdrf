de46bc8bc216ab93fe4b6289df5f85f51f0e358f
Priyanshu <priyanshushekhar100@gmail.com>
2024-09-07 10:00:30 +0530

    header changes

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
-// import logo from "./left.png"; // Adjust the path based on your folder structure
 import styles from "./Header.module.css"; // Import the CSS module
 
-function Header({ title }) {
+function Header() {
   return (
     <header className={styles.header}>
       {/* <img src={logo} alt="Logo" className={styles.logo} /> */}
-      <h1 className={styles.title}>{title}</h1>
+      <h1 className={styles.title}>Research Scholars</h1>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>
+          PhD scholars under CDRF (1st call)
+        </button>
+        <button className={styles.button}>
+          PhD scholars under CDRF (2nd call)
+        </button>
+        <a
+          href="https://sire.bits-pilani.ac.in/programs/research/phd-drive_1"
+          className={styles.button}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PhD Drive
+        </a>
+      </div>
     </header>
   );
 }
